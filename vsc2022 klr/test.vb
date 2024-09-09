4761b1455c5d44f0308630a6562a9e614e531b6f
kleroyrabelais <kleroypro22@gmail.com>
2024-09-09 15:04:04 +0200

    c'est dure le vb

---
+++ vsc2022 klr/test.vb
@@ -0,0 +1,10 @@
+﻿Module test
+    Sub Main()
+        Dim montant
+        Dim remise
+        Dim remises_apres_montant
+        Console.WriteLine("Veuillez taper votre montant")
+        If 2000 < 5000 Then
+    End Sub
+
+End Module
