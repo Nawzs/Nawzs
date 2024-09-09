6bff01cdb55da92c4ffde28c854c72a43e9c4724
kleroyrabelais <kleroypro22@gmail.com>
2024-09-09 15:15:31 +0200

    ajout pour 1 et 2%

--- vsc2022 klr/test.vb
+++ vsc2022 klr/test.vb
@@ -2,9 +2,13 @@
     Sub Main()
         Dim montant
         Dim remise
-        Dim remises_apres_montant
+        Dim resultat
         Console.WriteLine("Veuillez taper votre montant")
         If 2000 < 5000 Then
+            remise / 100
+            remise / 100 * 2
+
+        End If
     End Sub
 
 End Module
